c26f8197b37e3eb6a26b22e6111543522ffaf5c9
Shaderax <rouzeval1998@live.fr>
2017-04-13 19:34:49 +0200

    lib

--- libft/ft_memalloc.c
+++ libft/ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: nrouzeva <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 12:07:21 by nrouzeva          #+#    #+#             */
-/*   Updated: 2017/02/14 13:34:18 by nrouzeva         ###   ########.fr       */
+/*   Updated: 2017/04/10 11:07:35 by nrouzeva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	*ft_memalloc(size_t size)
 	}
 	else
 	{
-		ft_putstr_fd("Malloc Problem #fail lol", 2);
+		ft_putstr_fd("Malloc Problem #fail", 2);
 		exit(1);
 	}
 	return (ptr);
